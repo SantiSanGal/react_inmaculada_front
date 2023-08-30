972c6fb35b4bbcfcbc4952a6eebabf197395a49d
SantiSanGal <santiagon.g.m@gmail.com>
2023-08-29 22:17:05 -0400

    PageMenu

--- src/pages/page-menu/PageMenu.tsx
+++ src/pages/page-menu/PageMenu.tsx
@@ -1,6 +1,12 @@
+import './styles/pageMenu.css'
 
 export const PageMenu = () => {
   return (
-    <div>PageMenu</div>
+    <div className='pageMenu'>
+      <div className="options">Registrar Compras</div>
+      <div className="options">Verificar Stock</div>
+      <div className="options">Nueva Venta</div>
+      <div className="options">Reportes</div>
+    </div>
   )
 }
