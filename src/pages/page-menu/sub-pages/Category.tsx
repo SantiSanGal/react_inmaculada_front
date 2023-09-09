0ac4f16e882041645518ea64e9fa5804a5ff81c6
SantiSanGal <santiagon.g.m@gmail.com>
2023-09-08 22:08:02 -0400

    listing categories

--- src/pages/page-menu/sub-pages/Category.tsx
+++ src/pages/page-menu/sub-pages/Category.tsx
@@ -1,22 +1,63 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { inmacualdaApi } from "../../../api/inmaculadaApi"
 
-const getAllCategories = () => {
+const getAllCategories = (setCategory) => {
   inmacualdaApi.get('/category')
-    .then(res => console.log("res ", res))
+    .then(res => setCategory(res.data))
     .catch(err => console.log(err))
+}
+
+const handleClick = ( id: number, method: string ) => {
+  if (method == 'edit') {
+      
+  }else{
+      
+  }
 }
 
 export const Category = () => {
   
+  const [category, setCategory] = useState()  
+
   useEffect(() => {
-    getAllCategories()
+    getAllCategories(setCategory)
   }, [])
   
   
   return (
-    <div>
-      Category
+    <div className="subPage">
+        <table className="subPageCategory">
+            <thead>
+                <tr>
+                    <th>N°</th>
+                    <th>Category</th>
+                    <th>Descripción</th>
+                    <th>Estado</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    category?.data.map((item, i) => (
+                        <tr key={item.id}>
+                            <td>{i+1}</td>
+                            <td>{item.brand}</td>
+                            <td>{item.description}</td>
+                            <td>{item.status ? "Activo" : "Inactivo"}</td>
+                            <td className="botones">
+                                <button onClick={() => handleClick(item.id, 'edit')} className="btn edit">Edit</button>
+                                <button onClick={() => handleClick(item.id, 'del')} className="btn del">Del</button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colSpan={5}>Paginación</td>
+                </tr>
+            </tfoot>
+        </table>
     </div>
   )
 }
